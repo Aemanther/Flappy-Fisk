ef7cca093c4c6cb54e178f5426aa3aa62cc0f520
Sandra Moen <sandra77_6@hotmail.com>
2014-09-21 15:27:41 +0200

    Flappy Fisk
    First

---
+++ Flappy Fisk-desktop/src/com/slideshow/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.slideshow.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.slideshow.game.FlappyFisk;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyFisk(), config);
+	}
+}
