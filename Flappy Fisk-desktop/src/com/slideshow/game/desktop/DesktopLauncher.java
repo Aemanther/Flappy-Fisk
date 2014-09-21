eb1ae2cf08cdf4f117f856b887d18cf958783ac6
Sandra Moen <sandra77_6@hotmail.com>
2014-09-21 15:32:40 +0200

    test
    test

--- Flappy Fisk-desktop/src/com/slideshow/game/desktop/DesktopLauncher.java
+++ Flappy Fisk-desktop/src/com/slideshow/game/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.slideshow.game.FlappyFisk;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Flappy Fisk";
+		config.width = 360;
+		config.height = 640;
+		config.vSyncEnabled = true;
+		config.resizable = false;
+		
 		new LwjglApplication(new FlappyFisk(), config);
 	}
 }
